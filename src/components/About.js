11a785a222de33827971ad8fbbffb5bb80f0e4b1
Apoh <Edenra08@gmail.com>
2022-06-05 03:52:54 +0100

    Assignment Completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,42 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* <p>{props.bio}</p> */}
+  
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+    
+    <Links github={props.github}  linkedin={props.linkedin}/>
+
+
     </div>
   );
+}
+
+
+
+function BlogContent(props) {
+  console.log(props);
+
+  if (!props.isPublished) {
+    // hide unpublished content
+    // return null means "don't display any DOM elements here"
+    return null;
+  } else {
+    // show published content
+    return (
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+  }
 }
 
 export default About;
---
+++ src/components/Links.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Links(props){
+    return (
+    <div>
+        <h3>Links</h3>
+        <a href="github">https://github.com/liza</a> 
+        <a href="linkedin">https://www.linkedin.com/in/liza/</a>
+        </div>)
+    // links: {
+    //     github: "https://github.com/liza",
+    //     linkedin: "https://www.linkedin.com/in/liza/",
+    //   },
+    
+}
+
+
+export default Links;
